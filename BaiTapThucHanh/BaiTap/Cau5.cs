b549a5684ee68b7544a4dbdba8b82cc06aba5897
phanvandinh99 <vonhanh271@gail.com>
2021-10-19 23:50:09 +0700

    abc

--- BaiTapThucHanh/BaiTap/Cau5.cs
+++ BaiTapThucHanh/BaiTap/Cau5.cs
@@ -19,9 +19,32 @@ namespace BaiTap
 
         private void btnQuaySo_Click(object sender, EventArgs e)
         {
+            int diem = 0;
             Random rd = new Random();
-            
 
+            lbSo1.Text = (rd.Next(0, 9)).ToString();
+            lbSo2.Text = (rd.Next(0, 9)).ToString();
+            lbSo3.Text = (rd.Next(0, 9)).ToString();
+            // gán giá trị vào biến
+            int a = int.Parse(lbSo1.Text);
+            int b = int.Parse(lbSo2.Text);
+            int c = int.Parse(lbSo3.Text);
+
+            int Tong = (a + b + c);
+            if(Tong>=3 && Tong<=10)
+            {
+                diem = diem + 10;
+                lbDiem.Text = diem.ToString();
+            }
+            else
+            {
+
+            }
+            if (Tong >= 11 && Tong <= 18)
+            {
+                diem = diem + 10;
+                lbDiem.Text = diem.ToString();
+            }
         }
     }
 }
